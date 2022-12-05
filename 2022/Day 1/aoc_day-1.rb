bfa1dddcb6052b96bd870068f8d8194b33fb35bc
Neox471 <83208987+Neox471@users.noreply.github.com>
2022-12-06 00:31:22 +0100

    [TASK] AOC Day 3 Part 1 & Refinement Day 1

--- 2022/Day 1/aoc_day-1.rb
+++ 2022/Day 1/aoc_day-1.rb
@@ -5,6 +5,6 @@ elves = input.map { |input| input.split("\n").map(&:to_i) }
 # Calculate sum of each array
 calories_elves = elves.map { |elf| elf.sum}
 # Output sum of calories of the top elf [Answer part 1]
-p calories_elves.max
+p "Calories of the top elf: #{calories_elves.max}"
 # Output sum of calories of the top three elves [Answer part 2]
-p calories_elves.sort.last(3).sum
+p "Calories of the top three elves #{calories_elves.sort.last(3).sum}"
---
+++ 2022/Day 3/aoc_day-3.rb
@@ -0,0 +1,75 @@
+# Set score to 0
+score = 0
+# Define how much score each letter is worth
+letter_scores = {
+  'a' => 1,
+  'b' => 2,
+  'c' => 3,
+  'd' => 4,
+  'e' => 5,
+  'f' => 6,
+  'g' => 7,
+  'h' => 8,
+  'i' => 9,
+  'j' => 10,
+  'k' => 11,
+  'l' => 12,
+  'm' => 13,
+  'n' => 14,
+  'o' => 15,
+  'p' => 16,
+  'q' => 17,
+  'r' => 18,
+  's' => 19,
+  't' => 20,
+  'u' => 21,
+  'v' => 22,
+  'w' => 23,
+  'x' => 24,
+  'y' => 25,
+  'z' => 26, 
+  'A' => 27,
+  'B' => 28,
+  'C' => 29,
+  'D' => 30,
+  'E' => 31,
+  'F' => 32,
+  'G' => 33,
+  'H' => 34,
+  'I' => 35,
+  'J' => 36,
+  'K' => 37,
+  'L' => 38,
+  'M' => 39,
+  'N' => 40,
+  'O' => 41,
+  'P' => 42,
+  'Q' => 43,
+  'R' => 44,
+  'S' => 45,
+  'T' => 46,
+  'U' => 47,
+  'V' => 48,
+  'W' => 49,
+  'X' => 50,
+  'Y' => 51,
+  'Z' => 52, 
+}
+# Read file input and split each line
+input = File.read("advent-of-code_input.txt").split("\n")
+# Each line represents a rucksack so we iterate trough each rucksack
+input.each do |rucksack|
+    # Calculate half of each rucksack and build first and second compartment from it
+    half = (rucksack.length / 2)
+    first_compartment = rucksack[0..half-1]
+    second_compartment = rucksack[half..rucksack.length]
+    # Iterate each letter in first compartment and look if it is included in second compartment
+    first_compartment.split('').each do |letter|
+        if second_compartment.include?(letter)
+            score += letter_scores[letter]
+
+            break
+        end
+    end
+end
+puts score
